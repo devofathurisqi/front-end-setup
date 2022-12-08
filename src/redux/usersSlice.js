f09adf26489d436bc936f8b233dd99c5d9469096
devofathurisqi <devofathurisqi3@gmail.com>
2022-12-08 15:44:42 +0700

    frontend-setup

---
+++ src/redux/usersSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    
+};
+
+export const usersSlice = createSlice({
+    name: "counter",
+    initialState,
+    reducers: {
+        
+    },
+});
+
+export default usersSlice.reducer;
